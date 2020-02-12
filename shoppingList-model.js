ce2c32cf7c96a5f59269bbac46ddc27a1ac22cae
k1901142 <57808538+k1901142@users.noreply.github.com>
2020-02-12 23:06:15 +0200

    Ostoslista-taso

---
+++ shoppingList-model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const shoppingList_schema = new Schema({
+    nameShoppingList: {
+        type: String,
+        required: true
+    },
+    products: [{
+        type: Object,
+        req: true
+    }]
+});
+
+const shoppingList_model = mongoose.model('shoppingList', shoppingList_schema);
+
+module.exports = shoppingList_model;
