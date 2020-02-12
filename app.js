bd10e665cb541e5a6bc1d01b4b9c78c6be8c1f3e
k1901142 <57808538+k1901142@users.noreply.github.com>
2020-02-12 23:09:05 +0200

    Ostoslista-taso

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 //Controllers
 const auth_controller = require('./controllers/auth_controller');
 const shoppingList_controller = require('./controllers/shoppingList_controller');
+const product_controller = require('./controllers/product_controller');
 
 let app = express();
 
@@ -23,12 +24,16 @@ app.use(session({
     }
 }));
 
+//
 app.use((req, res, next) => {
+    console.log('App huutelee:');
     console.log(`${req.method} ${req.path}`);
     next();
 });
 
+//onko käyttäjä loggautunut sisään?
 const is_logged_handler = (req, res, next) => {
+    console.log('Onko käyttäjä sisällä?');
     if (!req.session.user) {
         return res.redirect('/login');
     }
@@ -39,10 +44,15 @@ const is_logged_handler = (req, res, next) => {
 app.use('/css', express.static('css'))
 
 //Auth
+console.log('Nyt app.use: handle_user');
 app.use(auth_controller.handle_user);
+console.log('Nyt app.get: get_login');
 app.get('/login', auth_controller.get_login);
+console.log('Nyt app.post: post_login');
 app.post('/login', auth_controller.post_login);
+console.log('Nyt app.post: post_register');
 app.post('/register', auth_controller.post_register);
+console.log('Nyt app.post: post_logout');
 app.post('/logout', auth_controller.post_logout);
 
 
@@ -51,6 +61,9 @@ app.get('/', is_logged_handler, shoppingList_controller.get_shoppingLists);
 app.post('/delete-shoppingList', is_logged_handler, shoppingList_controller.post_delete_shoppingList);
 app.get('/shoppingList/:id', is_logged_handler, shoppingList_controller.get_shoppingList);
 app.post('/add-shoppingList', is_logged_handler, shoppingList_controller.post_shoppingList);
+
+//Products
+
 
 app.use((req, res, next) => {
     res.status(404);
